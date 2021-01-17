2d9a9ecf0a5c75c8a9316a106dc7d25a8ba92371
Filip Matejko <matejkofilip@gmail.com>
2021-01-17 18:41:15 +0100

    added abstract factory pattern

---
+++ pattern_abstract_factory.py
@@ -0,0 +1,96 @@
+'''
+Abstract Factory pattern. Allows creating famillies of related products that share common interface.
+here: 
+    White Factory can create White Dogs and White Cats, Black Factory can create Black Dogs and Black Cats,
+    Cats and Dogs share interface of abstract class Animal
+'''
+class Animal():
+
+  def __init__(self):
+    pass
+
+  def __repr__(self):
+    answer = []
+    answer.append(self.__class__.__name__)
+    for i in self.__dict__.keys():
+      answer.append(f'{i}: {self.__dict__[i]}')
+    return '\n'.join(answer)
+
+class Cat(Animal):
+
+  def __init__(self, weight=3, hunger=5):
+    self.weight = weight
+    self.hunger = hunger
+
+  def meow(self):
+    print('MEOW')
+
+class WhiteCat(Cat):
+
+  def __init__(self, weight=3, hunger=5):
+    super().__init__(weight=weight, hunger=hunger)
+    self.colour = 'white'
+
+class BlackCat(Cat):
+
+  def __init__(self, weight=3, hunger=5):
+    super().__init__(weight=weight, hunger=hunger)
+    self.colour = 'black'
+
+class Dog(Animal):
+
+  def __init__(self, weight=25, hunger=5):
+    self.weight = weight
+    self.hunger = hunger
+
+  def bark(self):
+    print('WOOF')
+
+class WhiteDog(Dog):
+
+  def __init__(self, weight=25, hunger=5):
+    super().__init__(weight=weight, hunger=hunger)
+    self.colour = 'white'
+
+class BlackDog(Dog):
+
+  def __init__(self, weight=25, hunger=5):
+    super().__init__(weight=weight, hunger=hunger)
+    self.colour = 'black'
+
+
+class Factory():
+
+  @classmethod
+  def create(cls):
+    pass
+
+class WhiteFactory(Factory):
+
+  @classmethod
+  def create_white_dog(self, weight=3, hunger=5):
+    return WhiteDog(weight=weight, hunger=hunger)
+
+  @classmethod
+  def create_white_cat(self, weight=3, hunger=5):
+    return WhiteCat(weight=weight, hunger=hunger)
+
+class BlackFactory(Factory):
+
+  @classmethod
+  def create_black_cat(self, weight=3, hunger=5):
+    return BlackCat(weight=weight, hunger=hunger)
+
+  @classmethod
+  def create_black_dog(self, weight=3, hunger=5):
+    return BlackDog(weight=weight, hunger=hunger)
+
+'''#uncomment for demonstration
+
+whitecat = WhiteCatFactory.create()
+blackcat = BlackCatFactory.create()
+whitedog = WhiteDogFactory.create()
+blackdog = BlackDogFactory.create()
+
+print(whitedog)
+'''
