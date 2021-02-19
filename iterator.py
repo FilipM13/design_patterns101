300c69b81f2722ca0dc939100c2fd37b436bce78
Filip Matejko <matejkofilip@gmail.com>
2021-02-19 13:50:31 +0100

    Fixing pattern, adding other iterator.

--- iterator.py
+++ iterator.py
@@ -1,24 +1,36 @@
 """
 Iterator. Allows iterating in specific way through custom structure.
+Here are two implementations of iterator - internal and external. For no reason.
 here:
-  Tree (and subclasses) - structure (based on composite pattern)
+  Tree (and subclasses) - iterable structure (based on composite pattern)
+    __next__ method and inheriting from Iterator
   TreeIterator - class allowing to iterate through Tree structure
 """
 from abc import ABC
+from collections.abc import Iterator
 
 class Tree(ABC):
   pass
 
-class Branch(Tree):
+class Branch(Tree, Iterator):
 
   def __init__(self):
     self.elements = list()
+    self.position = 0
 
   def add_element(self, element):
     self.elements.append(element)
 
   def __repr__(self):
     return str(self.elements)
+
+  def __next__(self):
+    try:
+      rv = self.elements[self.position]
+      self.position += 1
+    except IndexError:
+      raise StopIteration
+    return rv
 
 class Leaf(Tree):
 
@@ -34,9 +46,10 @@ class Leaf(Tree):
   def __repr__(self):
     return str(self.value)
 
-class TreeIterator(ABC):
+class TreeIterator(Iterator):
 
   def __init__(self, tree: Branch):
+    self.position = 0
     self.tree = tree
     pass
 
@@ -45,6 +58,14 @@ class TreeIterator(ABC):
     for i, t in enumerate(self.tree.elements):
       rv += f'\nBranch {i}: ' + str(t)
     return rv+'\n]'
+
+  def __next__(self):
+    try:
+      rv = self.tree.elements[self.position]
+      self.position += 1
+    except IndexError:
+      raise StopIteration
+    return rv
 
 
 '''# uncomment for demonstration
@@ -68,9 +89,19 @@ b3.add_element(b2)
 b3.add_element(b2)
 b3.add_element(l1)
 
+print('\nShowing structure of iterable object:')
 iterator = TreeIterator(b3)
 print(iterator.show())
 
+print('\nShowing structure of iterable object:')
 b3.add_element(l1)
 print(iterator.show())
+
+print('\nIterating with internal iterator:')
+for i in b3:
+  print(i)
+
+print('\nIterating with external iterator:')
+for i in iterator:
+  print(i)
 '''
